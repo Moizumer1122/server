dd05b54d1bc8e93a8694486351126289d18c4d4d
moizumer1122 <umerbandesha007@gmail.com>
2025-11-26 17:56:12 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// Password123 password 
+// moiz umer username 
+// todo-mern db name 
+const express = require('express');
+const app =express();
+    
+const PORT =8000;
+app.listen(PORT,()=>{
+console.log('server is running on port :',PORT)
+})
