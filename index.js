e96825588f07b52ac021cf3bca03d7efe69e4656
moizumer1122 <umerbandesha007@gmail.com>
2025-11-27 17:53:34 +0500

    push

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 // Password123 password 
 // moiz umer username 
 // todo-mern db name 
+// to start the server use node index.js
 const express = require('express');
-const app =express();
-    
-const PORT =8000;
-app.listen(PORT,()=>{
-console.log('server is running on port :',PORT)
+const cors = require("cors");
+const app = express();
+app.use(cors())
+app.use(express.json())
+app.post("/", (req, res) => {
+    const { body } = req
+    console.log('body', body);
+    res.send("data received");
+})
+const PORT = 8000;
+app.listen(PORT, () => {
+    console.log('server is running on port :', PORT)
 })
